2bba6cfcef8052648f5b55db4748082eefd3fa2e
Adesina Temiloluwa <adesinatemiloluwa105@gmail.com>
2025-04-14 17:39:36 +0100

    update hangman script

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -8,39 +8,41 @@ words = word_list
 
 # computer chooses a random word
 chosen_word = random.choice(words)
-display = ['_'] * len(chosen_word)
-end_of_game = False
+display = ["-"] * len(chosen_word)
 lives = 5
-hint = 3
-print("Welcome to my hangman game")
-print(logo)
+print("Hello and welcome to my hangman game!")
+print(hangman_logo)
+end_of_game = False
 while not end_of_game:
-    # print(chosen_word)
-    # get the user to guess a letter
-    guess = input("Guess a letter: ").lower()
+    guess = input("I have chosen a word, can you guess a letter in the word? \n").lower()
     word_length = len(chosen_word)
-
     if guess in display:
-        print(f"psst! You've already guessed the word {guess}. Try again!")
+        print(f"You have guessed the letter before. Try again")
         
     for position in range(word_length):
-        # print(position)
         letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
             
     if guess not in chosen_word:
         lives -= 1
-        print(f"wrong guess, you have {lives} lives left")
-        print(stages[lives])
+        print(f"wrong guess. You have {lives} lives left")
+        print(hangman_stages[lives])
+        hint = input("Would you like to get an hint? (yes/no): ")
+        
+        if hint.lower() == "yes":
+            position = random.randint(0, len(chosen_word) - 1)
+            hint_letter = chosen_word[position]
+            display[position] = hint_letter
+        #print(display)
+        
         if lives == 0:
             end_of_game = True
-            print(f"You lose. The chosed word was {chosen_word}")
-    
+            print(f"You lose. The chosen word was {chosen_word}")
+            
     print(display)
-    if '_' not in display:
+    if "-" not in display:
         end_of_game = True
-        print("You win")
-
-print(f"Yay! The word is {''.join(chosen_word)}")
+        print("You won")
+        
+print(f"Yay, the word is {"".join(display)}")
